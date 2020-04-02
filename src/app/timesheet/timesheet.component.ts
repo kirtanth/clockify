2e57b4db76c17ec9c3c308421022b8b2cd73e18c
Kirtan <kirtanthakkar000@gmail.com>
2020-04-03 02:01:49 +0530

    Data send Fine Done Finally

--- src/app/timesheet/timesheet.component.ts
+++ src/app/timesheet/timesheet.component.ts
@@ -1,14 +1,8 @@
-import { Showtime } from './../showtime.model';
 import { Router } from '@angular/router';
 import { UserService } from './../user.service';
 import { Component, OnInit, EventEmitter, Output, NgModule, Injectable } from '@angular/core';
-import { format } from 'path';
 import * as moment from 'moment';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-
-
-
 
 interface project {
   value: string;
@@ -26,19 +20,27 @@ interface project {
 })
 export class TimesheetComponent implements OnInit {
 
-  getwork(){
-    
-  }
-  
-  projectForm : FormGroup = new FormGroup({
-    projectName: new FormControl(null,Validators.required)
-  })
 
-addToTask(){
-  console.log(this.projectForm.value.projectName)
-}
-//Taking Username
-  username:String='';
+  //Initialization-------------------------------------
+  timeLeft: number = 0;
+  timeLeftmm: number = 0;
+  timeLefthh: number = 0;
+  a: number; b: number; c: number;
+  interval;
+  buttona = true;
+  starttime: string
+  time
+  endtime
+  totaltime
+  //Initialization----------------------------------
+
+
+  getwork() {
+
+  }
+
+  //Taking Username
+  username: String = '';
 
   //Constructor-----------------------------------------------------------
   constructor(private _user: UserService, private _router: Router) {
@@ -51,35 +53,23 @@ addToTask(){
 
 
   //Adding Name
-  addName(data){
+  addName(data) {
     this.username = data.username;
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+
+
   }
 
-  //Timer Code
-  timeLeft: number = 0;
-  timeLeftmm: number = 0;
-  timeLefthh: number = 0;
-  a: number; b: number; c: number;
-  interval;
-  buttona = true;
-  starttime
-  time
-  endtime
+
 
   @Output() RecordTime = new EventEmitter()
 
   startTimer() {
-    this.addToTask()
     this.buttona = false;
     //Getting user start time
     this.starttime = moment().format("HH:mm:ss");
-
-    console.log('Your Starting time' + this.starttime)
-
-
     this.interval = setInterval(() => {
       if (this.timeLeft <= 60) {
         this.timeLeft++;
@@ -109,41 +99,59 @@ addToTask(){
     this.RecordTime.emit(this.c + ':' + this.b + ':' + this.a);
   }
 
-
-
-
   // STOP TIMER CODE-----------------------------------------------------------------
   pauseTimer() {
     this.buttona = true;
+
     //Getting User Stop Time
     this.endtime = moment().format("HH:mm:ss");
-
     this.totaltime = moment.utc(moment(this.endtime, "HH:mm:ss").diff(moment(this.starttime, "HH:mm:ss"))).format("HH:mm:ss");
-
     this.a = this.timeLeft;
     this.b = this.timeLeftmm
     this.c = this.timeLefthh;
     this.timeLefthh = 0;
     this.timeLeft = 0;
     this.timeLeftmm = 0;
+
     clearInterval(this.interval);
     this.time = 'You Worked For :' + this.c + ':' + this.b + ':' + this.a + ' Hours'
-    
+    this.projectForm.patchValue({
+      p_startTime: this.starttime,
+      p_endTime: this.endtime,
+      P_sessionTime:this.totaltime
+    });
+    this.p_form()
+
   }
-// STOP Timer End---------------------------------------------------------------
- totaltime
+  // STOP Timer End---------------------------------------------------------------
+
   thours
 
   // PROJECT DROPDOWN------------------------------------------
   projects: project[] = [
-    {value: 'project-0', viewValue: 'Project-1'},
-    {value: 'Project-2', viewValue: 'Project-2'},
-    {value: 'Project-3', viewValue: 'Project-3'}
+    { value: 'project-0', viewValue: 'Project-1' },
+    { value: 'Project-2', viewValue: 'Project-2' },
+    { value: 'Project-3', viewValue: 'Project-3' }
   ];
-  // cs=false
-  // createSelect(){
-  //   this.cs=true  
-  // }
-  
+
+  // ProjectForm FormGroup-------------------------------
+
+  projectForm: FormGroup = new FormGroup({
+
+    p_name: new FormControl(null),
+    p_title: new FormControl(null, Validators.required),
+    p_startTime: new FormControl(),
+    p_endTime: new FormControl(this.endtime),
+    P_sessionTime: new FormControl(this.totaltime)
+  })
+
+
+
+
+  p_form() {
+    console.log(this.projectForm.value)
+  }
+
+
 
 }
